3fa8b7d1c70197b74d73a915db1939cf5ae1ce92
Randi Firmansyah <randykelvin26@gmail.com>
2021-11-29 10:59:13 +0700

    dalam 1 file main.go

--- main.go
+++ main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
-	route := gin.Default()
+	router := gin.Default()
 
-	route.GET("/", rootHandler)
-	route.GET("/books/:id/:title", booksHandler)
-	route.GET("/query", queryHandler)
-	route.POST("/book", bookHandler)
+	router.GET("/", rootHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
+	router.POST("/book", bookHandler)
 
-	route.Run()
+	//TODO Versioning
+	v1 := router.Group("/v1")
+	v1.GET("/", rootV1Handler)
+
+	router.Run()
 }
 
 func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Randi Firmansyah",
+		"bio":  "Hello world",
+	})
+}
+
+func rootV1Handler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"name": "Randi Firmansyah",
 		"bio":  "Hello world",
@@ -40,9 +53,9 @@ func queryHandler(c *gin.Context) {
 }
 
 type BookInput struct {
-	Title    string
-	Price    int
-	Subtitle string `json:"sub_title"`
+	Title    string      `json:"title" binding:"required"`
+	Price    json.Number `json:"price" binding:"required,number"`
+	Subtitle string      `json:"sub_title" binding:"required"`
 }
 
 func bookHandler(c *gin.Context) {
@@ -50,7 +63,19 @@ func bookHandler(c *gin.Context) {
 	err := c.ShouldBindJSON(&book)
 
 	if err != nil {
-		log.Fatal()
+
+		errorMassages := []string{}
+
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMassage := fmt.Sprintf("Error on field: %s, condition: %s", e.Field(), e.ActualTag())
+			errorMassages = append(errorMassages, errorMassage)
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMassages,
+		})
+
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
